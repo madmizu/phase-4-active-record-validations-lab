af022bc85ab0128ff1cd62d56e5ed211bb23e937
Madison Shimizu <mhshimizu@outlook.com>
2022-01-17 09:04:27 -0700

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :title_is_clickbait?
+
+    CLICKBAIT_TEXT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_is_clickbait?
+        if CLICKBAIT_TEXT.none? { |pattern| pattern.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
